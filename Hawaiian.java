5b116994c0f1b99114a2c3a51b945d7d2f6853d0
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-11-12 22:02:48 -0500

    Update Hawaiian.java

--- Hawaiian.java
+++ Hawaiian.java
@@ -2,7 +2,34 @@ package application;
 
 public class Hawaiian extends Pizza {
 
-	
+	public Hawaiian() {
+		
+	}
+	@Override
+	public void calculatePrice() {
+		double price = 0;
+		
+		//small is $10.99, medium is $12.99, large is $14.99
+		//$1.49 for each additional topping, up to 2 toppings
+		if (String.valueOf(this.getSize()).equals("SMALL")) {
+			price = 10.99;
+		} else if (String.valueOf(this.getSize()).equals("MEDIUM")) {
+			price = 12.99;
+		} else {
+			price = 14.99;
+		}
+		
+		if (this.getNoToppings() > 2) {
+			int i = this.getNoToppings() - 2;
+			while (i > 0) {
+				price += 1.49;
+				i--;
+				
+			}
+		}
+
+		this.setPrice(price);
+	}
 	@Override 
 	public String toString() {
 		String str = super.toString();
