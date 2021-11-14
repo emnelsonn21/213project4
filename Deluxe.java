6765c29c39d7b1c4f273e89affafe038111b6063
cris431 <32299513+cris431@users.noreply.github.com>
2021-11-14 12:29:58 -0500

    Update Deluxe.java

--- Deluxe.java
+++ Deluxe.java
@@ -1,14 +1,35 @@
 package application;
-
+/**
+This class defines the type Deluxe, which is an extenstion of Pizza, with all of its attributes and methods
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public class Deluxe extends Pizza {
-
+	
+	/**
+	Empty constructor for a Deluxe pizza
+	@author Emily Nelson
+	*/
 	public Deluxe() {
 		
 	}
+	
+	/**
+	Constructor for a Deluxe pizza
+	Creates a type Pizza for this student
+	@param orderNumber the order number to set 
+	@param size the size of pizza to set
+	@param toppings the toppings of the pizza to set
+	@param price the price of the pizza to set
+	@author Emily Nelson
+	*/
 	public Deluxe(String orderNumber, Size size, Toppings[] toppings, double price) {
 		super(orderNumber, size, toppings, price);
 	}
 	
+	/**
+	Calculates the price of a Deluxe pizza
+	@author Emily Nelson
+	*/
 	@Override
 	public void calculatePrice() {
 		double price = 0;
@@ -35,6 +56,11 @@ public class Deluxe extends Pizza {
 		this.setPrice(price);
 	}
 	
+	/**
+	Returns the pizza in string form
+	@return textual represential of pizza 
+	@author Emily Nelson
+	*/
 	@Override
 	public String toString() {
 		String str = super.toString();
