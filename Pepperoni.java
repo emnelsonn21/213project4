c7a66c028c3c8975c130950e73cdea56eb26e7b7
cris431 <32299513+cris431@users.noreply.github.com>
2021-11-14 15:50:20 -0500

    Update Pepperoni.java

--- Pepperoni.java
+++ Pepperoni.java
@@ -1,14 +1,35 @@
 package application;
-
+/**
+This class defines the type Pepperoni, which is an extenstion of Pizza, with all of its attributes and methods
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public class Pepperoni extends Pizza {
-
+	
+	/**
+	Empty constructor for a Pepperoni pizza
+	@author Emily Nelson
+	*/
 	public Pepperoni() {
 		
 	}
+	
+	/**
+	Constructor for a Pepperoni pizza
+	Creates a type Pizza for this student
+	@param orderNumber the order number to set 
+	@param size the size of pizza to set
+	@param toppings the toppings of the pizza to set
+	@param price the price of the pizza to set
+	@author Emily Nelson
+	*/
 	public Pepperoni(String orderNumber, Size size, Toppings[] toppings, double price) {
 		super(orderNumber, size, toppings, price);
 	}
 	
+	/**
+	Calculates the price of a Pepperoni pizza
+	@author Emily Nelson
+	*/
 	@Override
 	public void calculatePrice() {
 		double price = 0;
@@ -33,6 +54,11 @@ public class Pepperoni extends Pizza {
 		this.setPrice(price);
 	}
 	
+	/**
+	Returns the pizza in string form
+	@return textual represential of pizza 
+	@author Emily Nelson
+	*/
 	@Override
 	public String toString() {
 		String str = super.toString();
