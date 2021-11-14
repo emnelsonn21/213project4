0054ec138e051fe16304e061743f6b2a9f1f7ca0
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-11-14 17:28:49 -0500

    Update Pizza.java

--- Pizza.java
+++ Pizza.java
@@ -1,88 +1,42 @@
 package application;
 
 import java.text.DecimalFormat;
+import java.util.ArrayList;
 
 public abstract class Pizza {
-	
-	public void calculatePrice() {
-	}
-
-	private String orderNumber;
-	private Size size;
-	private Toppings[] toppings;
-	private int noToppings;
-	private double price;
+	protected ArrayList<Toppings> toppings = new ArrayList<Toppings>();
+	protected Size size;
+	public abstract double price();
 	
 	
 	public Pizza() {
 		
 	}
 	
-	public Pizza(String orderNumber) {
-		this.orderNumber = orderNumber;
-	}
-	
-	public Pizza(String orderNumber, Size size, Toppings[] toppings, double price) {
-		this.orderNumber = orderNumber;
-		this.size = size;
-		this.toppings = toppings;
-		this.price = price;
-	}
-	
 	
 	@Override
 	public String toString() {
 		String theToppings = "";
-		for (int i = 0; i < toppings.length; i++) {
-			if (toppings[i] != null) {
-				theToppings += toppings[i] + ", ";
+		for (int i = 0; i < toppings.size(); i++) {
+			if (toppings.get(i) != null) {
+				theToppings += toppings.get(i) + ", ";
 			}
 		}
 		
 		DecimalFormat df = new DecimalFormat("0.00");
 		
-		return String.valueOf(size) + " pizza with " + theToppings + " for " + df.format(price);
+		return String.valueOf(size) + " pizza with " + theToppings;
 	}
 	
-	
-	public String getOrderNumber() {
-		return orderNumber;
-	}
-	
-	public void setOrderNumber(String orderNumber) {
-		this.orderNumber = orderNumber;
-	}
-	
-	public Size getSize() {
-		return size;
-	}
 	
 	public void setSize(Size size) {
 		this.size = size;
 	}
+		
 	
-	public double getPrice() {
-		return price;
-	}
-	
-	public void setPrice(double price) {
-		this.price = price;
-	}
-	
-	public Toppings[] getToppings() {
-		return toppings;
-	}
-	
-	public void setToppings(Toppings[] toppings) {
+	public void setToppings(ArrayList<Toppings> toppings) {
 		this.toppings = toppings;
 	}
-	
-	public int getNoToppings() {
-		return noToppings;
-	}
-	
-	public void setNoToppings(int noToppings) {
-		this.noToppings = noToppings;
-	}
+
 }
 
