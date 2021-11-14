1f2085d88592819ff076b3e014112023624542d4
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-11-14 17:49:55 -0500

    Update Deluxe.java

--- Deluxe.java
+++ Deluxe.java
@@ -3,36 +3,35 @@ package application;
 import java.text.DecimalFormat;
 
 /**
-This class defines the type Deluxe, which is an extenstion of Pizza, with all of its attributes and methods
+This class defines the type Hawaiian, which is an extenstion of Pizza, with all of its attributes and methods
 @author Emily Nelson, Cristofer Gomez-Martinez
 */
-public class Deluxe extends Pizza {
+public class Hawaiian extends Pizza {
 
-	static final double smallPrice = 12.99;
-	static final double medPrice = 14.99;
-	static final double largePrice = 16.99;
+	static final double smallPrice = 10.99;
+	static final double medPrice = 12.99;
+	static final double largePrice = 14.99;
 	static final double toppingPrice = 1.49;
-	static final int defaultToppings = 5;
+	static final int defaultToppings = 2;
 	
 	/**
-	Empty constructor for a Deluxe pizza
+	Empty constructor for a Hawaiian pizza
 	@author Emily Nelson
 	*/
-	public Deluxe() {
+	public Hawaiian() {
 		
 	}
 	
-	
 	/**
-	Calculates the price of a Deluxe pizza
+	Calculates the price of a Hawaiian pizza
 	@author Emily Nelson
 	*/
 	@Override
 	public double price() {
 		double price = 0;
 		
-		//small is $12.99, medium is $14.99, large is $16.99
-		//$1.49 for each additional topping, up to 7 toppings
+		//small is $10.99, medium is $12.99, large is $14.99
+		//$1.49 for each additional topping, up to 2 toppings
 		if (String.valueOf(this.size).equals("SMALL")) {
 			price = smallPrice;
 		} else if (String.valueOf(this.size).equals("MEDIUM")) {
@@ -49,23 +48,23 @@ public class Deluxe extends Pizza {
 				
 			}
 		}
-
+		
 		DecimalFormat df = new DecimalFormat("0.00");
     	
     	price = Double.valueOf(df.format(price));
-
+		
 		return price;
 	}
 	
 	/**
 	Returns the pizza in string form
-	@return textual representation of pizza 
+	@return textual represential of pizza 
 	@author Emily Nelson
 	*/
-	@Override
+	@Override 
 	public String toString() {
 		String str = super.toString();
-		return "DELUXE : " + str;
+		return "HAWAIIAN : " + str;
 	}
-
+	
 }
