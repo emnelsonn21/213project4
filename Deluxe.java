acf586653f5b01b856d3f7183661cabf54f36c03
emnelsonn21 <55959987+emnelsonn21@users.noreply.github.com>
2021-11-14 17:49:32 -0500

    Update Deluxe.java

--- Deluxe.java
+++ Deluxe.java
@@ -8,8 +8,11 @@ This class defines the type Deluxe, which is an extenstion of Pizza, with all of
 */
 public class Deluxe extends Pizza {
 
-	private String orderNumber;
-	private double thePrice;
+	static final double smallPrice = 12.99;
+	static final double medPrice = 14.99;
+	static final double largePrice = 16.99;
+	static final double toppingPrice = 1.49;
+	static final int defaultToppings = 5;
 	
 	/**
 	Empty constructor for a Deluxe pizza
@@ -31,17 +34,17 @@ public class Deluxe extends Pizza {
 		//small is $12.99, medium is $14.99, large is $16.99
 		//$1.49 for each additional topping, up to 7 toppings
 		if (String.valueOf(this.size).equals("SMALL")) {
-			price = 12.99;
+			price = smallPrice;
 		} else if (String.valueOf(this.size).equals("MEDIUM")) {
-			price = 14.99;
+			price = medPrice;
 		} else {
-			price = 16.99;
+			price = largePrice;
 		}
 		
-		if (this.toppings.size() > 5) {
-			int i = this.toppings.size() - 5;
+		if (this.toppings.size() > defaultToppings) {
+			int i = this.toppings.size() - defaultToppings;
 			while (i > 0) {
-				price += 1.49;
+				price += toppingPrice;
 				i--;
 				
 			}
@@ -51,7 +54,6 @@ public class Deluxe extends Pizza {
     	
     	price = Double.valueOf(df.format(price));
 
-		this.thePrice = price;
 		return price;
 	}
 	
