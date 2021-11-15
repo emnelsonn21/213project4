7931fc1d9f3188749a786d27601f46bcf997a315
cris431 <32299513+cris431@users.noreply.github.com>
2021-11-14 22:14:12 -0500

    Update Size.java

--- Size.java
+++ Size.java
@@ -1,5 +1,9 @@
 package application;
 
+/**
+This class defines the object Size and lists all possible sizes
+@author Emily Nelson, Cristofer Gomez-Martinez
+*/
 public enum Size {
 	SMALL, MEDIUM, LARGE
 }
